d931b47f0199b858eb2f9c8a7fb16e03379677eb
caseycantrell <cantrellcasey@gmail.com>
2022-03-05 13:43:24 -0600

    adds dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="navbar">
+        
+      </div>
+      <div className="main"> 
+          
+      </div>
+      <div className="footer">
+        
+      </div>
     </div>
   );
 }
