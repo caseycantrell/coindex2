36323fa929999bb629ef22c832b9886cbd316a1d
caseycantrell <cantrellcasey@gmail.com>
2022-03-06 19:26:39 -0600

    adds search bar and filtering

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
@@ -8,11 +8,25 @@ const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100;
     const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+
+      const filteredData = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+      setCryptos(filteredData);
+      
+    }, [cryptoList, searchTerm]); 
+
+    if(isFetching) return "Loading..."
 
     return (
       <>
+      <div className="search-crypto">
+        <Input placeholder="Search cryptocurrency..." onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div>
       <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
             <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}> 
                 <Link to={`/crypto/${currency.id}`}>
                     <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} alt="icon" />} hoverable>
